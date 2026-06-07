269223616d6149d633232c0ed9e73e951749622e
Laith Freije <katadafreije@gmail.com>
2026-06-07 14:52:24 -0400

    Initialize OLED driver

--- src/drivers/oled.rs
+++ src/drivers/oled.rs
@@ -2,9 +2,11 @@ use core::cell::RefCell;
 
 use embedded_hal::i2c::{AddressMode, Error, ErrorType, I2c};
 
+const OLED_ADDRESS: u8 = 0x3C;
+
 enum OLEDControlBytes {
     CommandFlow = 0x00,
-    DataFlow = 0x40
+    DataFlow = 0x40,
 }
 
 enum OLEDCommands {
@@ -13,7 +15,7 @@ enum OLEDCommands {
     ChargePumpSelect = 0x8D,
     AddressMode = 0x20,
     ColumnRange = 0x21,
-    PageRange = 0x22
+    PageRange = 0x22,
 }
 
 enum OLEDValues {
@@ -22,19 +24,22 @@ enum OLEDValues {
     VerticalAddressMode = 0x01,
 }
 
-pub struct OLED<'a, I>{
+pub struct OLED<'a, I> {
     i2c: &'a RefCell<I>,
-    address: u8,
 }
 
 impl<'a, I: embedded_hal::i2c::I2c> OLED<'a, I> {
-    fn send_command(&mut self, command: OLEDCommands, values: &[u8]) -> Result<(), <I as ErrorType>::Error> {
+    fn send_command(
+        &mut self,
+        command: OLEDCommands,
+        values: &[u8],
+    ) -> Result<(), <I as ErrorType>::Error> {
         let mut buf = [0u8; 8];
-        buf[0] = OLEDControlBytes::CommandFlow as u8;                  
+        buf[0] = OLEDControlBytes::CommandFlow as u8;
         buf[1] = command as u8;
         let len = 2 + values.len();
         buf[2..len].copy_from_slice(values);
-        self.i2c.borrow_mut().write(self.address, &buf)
+        self.i2c.borrow_mut().write(OLED_ADDRESS, &buf)
     }
 
     fn send_data(&mut self, values: &[u8]) -> Result<(), <I as ErrorType>::Error> {
@@ -42,17 +47,26 @@ impl<'a, I: embedded_hal::i2c::I2c> OLED<'a, I> {
         buf[0] = OLEDControlBytes::DataFlow as u8;
         let len = 1 + values.len();
         buf[1..len].copy_from_slice(values);
-        self.i2c.borrow_mut().write(self.address, &buf)
+        self.i2c.borrow_mut().write(OLED_ADDRESS, &buf)
     }
 
-    pub fn new(i2c: &'a RefCell<I>, address: u8) -> Self {
+    pub fn new(i2c: &'a RefCell<I>) -> Self {
         // Turn on charge pump
-        let mut oled = Self{i2c, address};
+        let mut oled = Self { i2c };
 
         oled.send_command(OLEDCommands::DisplayOff, &[]).unwrap();
-        oled.send_command(OLEDCommands::ChargePumpSelect, &[OLEDValues::ChargePumpOn as u8]).unwrap();
-        oled.send_command(OLEDCommands::AddressMode, &[OLEDValues::HorizontalAddressMode as u8]).unwrap();
-        oled.send_command(OLEDCommands::ColumnRange, &[0, 127]).unwrap();
+        oled.send_command(
+            OLEDCommands::ChargePumpSelect,
+            &[OLEDValues::ChargePumpOn as u8],
+        )
+        .unwrap();
+        oled.send_command(
+            OLEDCommands::AddressMode,
+            &[OLEDValues::HorizontalAddressMode as u8],
+        )
+        .unwrap();
+        oled.send_command(OLEDCommands::ColumnRange, &[0, 127])
+            .unwrap();
         oled.send_command(OLEDCommands::PageRange, &[0, 7]).unwrap();
         oled.send_command(OLEDCommands::DisplayOn, &[]).unwrap();
         oled
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 #![no_std]
 #![no_main]
 
+use core::cell::RefCell;
+
 use cortex_m_rt::entry;
 use embedded_hal::digital::OutputPin;
 use hal::pac;
 use panic_halt as _;
-use rp2040_hal::{self as hal, Clock};
+use rp2040_hal::{self as hal, fugit, Clock, I2C};
+
+use crate::drivers::oled::OLED;
 
 mod drivers;
 
@@ -14,6 +18,7 @@ mod drivers;
 pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
 
 const XOSC_CRYSTAL_FREQ: u32 = 12_000_000;
+const I2C_BUS_FREQUENCY_KHZ: u32 = 100;
 
 #[entry]
 fn main() -> ! {
@@ -40,6 +45,20 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
     let mut led = pins.gpio25.into_push_pull_output();
+
+    let i2c_bus = I2C::i2c0(
+        pac.I2C0,
+        pins.gpio16.reconfigure(),
+        pins.gpio17.reconfigure(),
+        fugit::RateExtU32::kHz(I2C_BUS_FREQUENCY_KHZ),
+        &mut pac.RESETS,
+        fugit::RateExtU32::Hz(XOSC_CRYSTAL_FREQ),
+    );
+
+    let refcell_i2c = RefCell::new(i2c_bus);
+
+    let oled_driver = OLED::new(&refcell_i2c);
+
     loop {
         led.set_high().unwrap();
         delay.delay_ms(500);
