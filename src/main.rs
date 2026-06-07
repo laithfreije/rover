58673ad397a70018bd006f4061feb4c93cb551e1
Laith Freije <katadafreije@gmail.com>
2026-06-07 12:41:02 -0400

    ci: Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+#![no_std]
+#![no_main]
+
+use cortex_m_rt::entry;
+use embedded_hal::digital::OutputPin;
+use hal::pac;
+use panic_halt as _;
+use rp2040_hal::{self as hal, Clock};
+
+#[link_section = ".boot2"]
+#[used]
+pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
+
+const XOSC_CRYSTAL_FREQ: u32 = 12_000_000;
+
+#[entry]
+fn main() -> ! {
+    let mut pac = pac::Peripherals::take().unwrap();
+    let core = pac::CorePeripherals::take().unwrap();
+    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
+    let clocks = hal::clocks::init_clocks_and_plls(
+        XOSC_CRYSTAL_FREQ,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .ok()
+    .unwrap();
+    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+    let sio = hal::Sio::new(pac.SIO);
+    let pins = hal::gpio::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+    let mut led = pins.gpio25.into_push_pull_output();
+    loop {
+        led.set_high().unwrap();
+        delay.delay_ms(500);
+        led.set_low().unwrap();
+        delay.delay_ms(500);
+    }
+}
